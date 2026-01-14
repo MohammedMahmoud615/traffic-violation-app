d73b779ddc86d3d7e3e7a0230251a749a9767a91
MohammedMahmoud615 <mohammed.mahmoud.abdu.alwadoud@gmail.com>
2026-01-14 14:34:44 +0200

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:untitled5/1.dart';
 import 'package:untitled5/all/Login.dart';
-import 'package:untitled5/all/list.dart';
 
 
 void main() {
@@ -19,7 +17,7 @@ void main() {
       ],
       theme: ThemeData(
     useMaterial3: true,
-    fontFamily: 'MyCustomFont', // تأكد أن الاسم يطابق الـ family في pubspec
+    fontFamily: 'MyCustomFont', 
   ),
       home: LoginPage(),
     ),
